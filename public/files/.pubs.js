f2a550af4db1037a8e62b42eeea543722a11f1ed
davidmichaelhogan <davidmichaelhogan@gmail.com>
2017-06-08 16:33:31 -0400

    patent shiz

---
+++ public/files/.pubs.js
@@ -0,0 +1,8 @@
+   <!-- Apollo Ad Script: Example Pub -->
+        <script type="text/javascript">
+          window.ApolloOptions = {
+            publisher: '58b86e1943a2e8534343b561'
+          }
+        </script>
+        <script type="text/javascript" async src='http://server.launchapollo.com/apollo.js'></script>
+   <!-- End Apollo Script -->
