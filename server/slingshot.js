990bbdcc1c7ee0a4529caadea9cab2b72b740eea
davidmichaelhogan <davidmichaelhogan@gmail.com>
2017-06-12 18:56:00 -0400

    this

--- server/slingshot.js
+++ server/slingshot.js
@@ -3,19 +3,19 @@ Slingshot.fileRestrictions( "uploadToAmazonS3", {
   maxSize: 1 * 1024 * 1024
 })
 
-Slingshot.createDirective( "newOne", Slingshot.S3Storage, {
-  bucket: "apolloserver-logos",
-  acl: "public-read",
-  authorize: function () {
-    // do some validation
-    // e.g. deny uploads if user is not logged in.
-    if (!this.userId) {
-      throw new Meteor.Error(403, "Login Required");
-     }
-
-    return true;
-  },
-  key: function ( file ) {
-    return this.userId + '/' + file.name
-  }
-})
+// Slingshot.createDirective( "uploadToAmazonS3", Slingshot.S3Storage, {
+//   bucket: "apolloserver-logos",
+//   acl: "public-read",
+//   authorize: function () {
+//     // do some validation
+//     // e.g. deny uploads if user is not logged in.
+//     if (!this.userId) {
+//       throw new Meteor.Error(403, "Login Required");
+//      }
+//
+//     return true;
+//   },
+//   key: function ( file ) {
+//     return this.userId + '/' + file.name
+//   }
+// })
