f6d11865a5c25de3742372bf53a6f4850761a4f4
davidmichaelhogan <davidmichaelhogan@gmail.com>
2017-06-12 17:54:42 -0400

    this

--- server/slingshot.js
+++ server/slingshot.js
@@ -1,22 +1,22 @@
-// Slingshot.fileRestrictions( "uploadToAmazonS3", {
-//   allowedFileTypes: [ "image/png", "image/jpeg" ],
-//   maxSize: 1 * 1024 * 1024
-// })
-//
-// Slingshot.createDirective( "uploadToAmazonS3", Slingshot.S3Storage, {
-//   bucket: "apolloserver-logos",
-//   acl: "public-read",
-//   authorize: function () {
-//     // do some validation
-//     // e.g. deny uploads if user is not logged in.
-//     if (!this.userId) {
-//       throw new Meteor.Error(403, "Login Required");
-//      }
-//
-//     return true;
-//   },
-//   key: function ( file ) {
-//     var user = Meteor.users.findOne( this.userId )
-//     return user.emails[0].address + "/" + file.name
-//   }
-// })
+Slingshot.fileRestrictions( "uploadToAmazonS3", {
+  allowedFileTypes: [ "image/png", "image/jpeg" ],
+  maxSize: 1 * 1024 * 1024
+})
+
+Slingshot.createDirective( "uploadToAmazonS3", Slingshot.S3Storage, {
+  bucket: "apolloserver-logos",
+  acl: "public-read",
+  authorize: function () {
+    // do some validation
+    // e.g. deny uploads if user is not logged in.
+    if (!this.userId) {
+      throw new Meteor.Error(403, "Login Required");
+     }
+
+    return true;
+  },
+  key: function ( file ) {
+    var user = Meteor.users.findOne( this.userId )
+    return user.emails[0].address + "/" + file.name
+  }
+})
