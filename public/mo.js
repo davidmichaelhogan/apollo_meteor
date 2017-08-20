db9cdeb6e1802cee54163f3e4ccb888305dd0c7f
cr-ks <davidmichaelandco@gmail.com>
2017-08-20 00:22:36 -0400

    add mo

--- public/mo.js
+++ public/mo.js
@@ -32,8 +32,7 @@ class MO {
 
     this.ads =  [
       "<ins class='adbladeads' data-cid='32965-3063715480' data-host='web.adblade.com' data-tag-type='4' data-protocol='https' style='display:none'></ins><script async src='https://web.adblade.com/js/ads/async/show.js' type='text/javascript'></script>",
-      "<ins class='adbladeads' data-cid='32965-3063715480' data-host='web.adblade.com' data-tag-type='4' data-protocol='https' style='display:none'></ins><script async src='https://web.adblade.com/js/ads/async/show.js' type='text/javascript'></script>",
-      "<script data-cfasync='false' type='text/javascript' src='//p236923.clksite.com/adServe/banners?tid=236923_449547_0'></script>"
+      "<ins class='adbladeads' data-cid='32965-3063715480' data-host='web.adblade.com' data-tag-type='4' data-protocol='https' style='display:none'></ins><script async src='https://web.adblade.com/js/ads/async/show.js' type='text/javascript'></script>"
     ]
 
     this.rand = (min,max) => {
