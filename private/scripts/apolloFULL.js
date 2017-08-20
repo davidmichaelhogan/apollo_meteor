e6d2d60b19472989d8312e76c426d618a3e8b4db
cr-ks <davidmichaelandco@gmail.com>
2017-08-20 16:20:07 -0400

    add mo

--- private/scripts/apolloFULL.js
+++ private/scripts/apolloFULL.js
@@ -149,7 +149,7 @@ class Apollo {
 
   showAd (ad) {
 
-    const html = `<a href="${this.click}/${this.env}/${window.ApolloOptions.publisher}/${ad.id}" target="_blank" style="display: block; width: 100%; text-decoration: none; font-family: arial, sans-serif; font-size: 20px;">
+    const html = `<a href="${this.api}/click?publisher=${window.ApolloOptions.publisher}&id=${ad.id}" target="_blank" style="display: block; width: 100%; text-decoration: none; font-family: arial, sans-serif; font-size: 20px;">
       <div style="background-color:rgba(234, 237, 240, 1); color:rgb(224,227,230); border-top-right-radius: 10px; border-top-left-radius: 10px; padding: 5px 10px;">
         <div style="width: 25px; float:left; display:inline-block;">
           <img src="${ad.logo}" style="max-width: 100%; max-height: 25px;">
