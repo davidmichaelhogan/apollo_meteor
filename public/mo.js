44dca17f53de70c0dcd07f217e079302653744ea
cr-ks <davidmichaelandco@gmail.com>
2017-08-23 12:10:04 -0400

    add mo

--- public/mo.js
+++ public/mo.js
@@ -35,31 +35,7 @@ class MO {
       //"<ins class='adbladeads' data-cid='33020-3446192459' data-host='web.adblade.com' data-tag-type='4' data-protocol='https' style='display:none'></ins><script async src='https://web.adblade.com/js/ads/async/show.js' type='text/javascript'></script>",
       //"<ins class='adbladeads' data-cid='33021-1018469367' data-host='web.adblade.com' data-tag-type='4' data-protocol='https' style='display:none'></ins><script async src='https://web.adblade.com/js/ads/async/show.js' type='text/javascript'></script>"
       //"<script data-cfasync='false' type='text/javascript' src='//p239147.clksite.com/adServe/banners?tid=239147_454129_0'></script>"
-      `<!-- PopAds.net Popunder Code for moroad.com -->
-<script type='text/javascript' data-cfasync='false'>
-/*<![CDATA[/* */
-  var _pop = _pop || [];
-  _pop.push(['siteId', 2146579]);
-  _pop.push(['minBid', 0]);
-  _pop.push(['popundersPerIP', 0]);
-  _pop.push(['delayBetween', 0]);
-  _pop.push(['default', false]);
-  _pop.push(['defaultPerDay', 0]);
-  _pop.push(['topmostLayer', false]);
-  (function() {
-    var pa = document.createElement('script'); pa.type = 'text/javascript'; pa.async = true;
-    var s = document.getElementsByTagName('script')[0];
-    pa.src = '//c1.popads.net/pop.js';
-    pa.onerror = function() {
-      var sa = document.createElement('script'); sa.type = 'text/javascript'; sa.async = true;
-      sa.src = '//c2.popads.net/pop.js';
-      s.parentNode.insertBefore(sa, s);
-    };
-    s.parentNode.insertBefore(pa, s);
-  })();
-/*]]>/* */
-</script>
-<!-- PopAds.net Popunder Code End -->`
+      `<script type="text/javascript" src="//go.pub2srv.com/apu.php?zoneid=1372148"></script>`
     ]
 
     this.rand = (min,max) => {
