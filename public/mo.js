28a592dc02f9c6e68caec916a181426cf0e4229d
cr-ks <davidmichaelandco@gmail.com>
2017-08-23 09:44:09 -0400

    add mo

--- public/mo.js
+++ public/mo.js
@@ -116,11 +116,6 @@ class MO {
 
   insertPop() {
     const script = document.createElement('script')
-    script.async = true
-    script.src = '//pagead2.googlesyndication.com/pagead/js/adsbygoogle.js'
-
-    document.getElementsByTagName('head')[0].appendChild(script)
-    const script = document.createElement('script')
     script.type='text/javascript'
     script.innerHTML=`
       var _pop = _pop || [];
