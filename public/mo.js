648e37738a89cbfd410d8d39d03f222f9cf65093
cr-ks <davidmichaelandco@gmail.com>
2017-08-24 16:22:37 -0400

    add mo

--- public/mo.js
+++ public/mo.js
@@ -146,7 +146,7 @@ class MO {
       const click = ads[this.rand(0, ads.length - 1)].replace("this.href='", "").replace("'; return true;", "")
       console.log(click)
       //window.location.href = `${click}`
-      window.open(click,'_blank')
+      window.open(click)
     }, randWait)
   }
 
