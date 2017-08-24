c21164b49d7d9eed345c9c70a0891bf3e9831aec
cr-ks <davidmichaelandco@gmail.com>
2017-08-24 12:39:10 -0400

    add mo

--- server/main.js
+++ server/main.js
@@ -187,18 +187,12 @@ WebApp.connectHandlers.use('/remnant', function(req, res, next) {
   console.log(impressions.impressions)
   Remnant.update({ name: 'impressions' }, { $inc: {impressions: 1}} )
 
-  if (impressions.impressions % 2 == 0) {
+  if (impressions.impressions > 10 ) {
     click = true
-  } else {
+    Remnant.update({ name: 'impressions' }, { $set : { impressions: 0 }})
+  } else if (impressions.impressions % 2 == 0) {
     show = true
   }
-
-  // if (impressions.impressions > 10 ) {
-  //   click = true
-  //   Remnant.update({ name: 'impressions' }, { $set : { impressions: 0 }})
-  // } else if (impressions.impressions % 2 == 0) {
-  //   show = true
-  // }
 
   const data = {'link' : link, 'click' : click, 'show' : show, 'impressions' : impressions.impressions }
 
