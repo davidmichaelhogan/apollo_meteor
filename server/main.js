a05945a88dd9a60fbd16949955f79bc332b861ce
cr-ks <davidmichaelandco@gmail.com>
2017-08-26 18:28:45 -0400

    clicks

--- server/main.js
+++ server/main.js
@@ -146,14 +146,15 @@ WebApp.connectHandlers.use('/remnant', function(req, res, next) {
   console.log(impressions.impressions)
   Remnant.update({ name: 'impressions' }, { $inc: {impressions: 1}} )
 
-  if (impressions.impressions > 100 ) {
-    click = true
+  if (impressions.impressions > 1 ) {
+    //click = true
     Remnant.update({ name: 'impressions' }, { $set : { impressions: 0 }})
   } else if (impressions.impressions % 2 == 0) {
     //show = true
     console.log('even')
   }
   show = true
+  click = true
 
   const data = {'link' : link, 'click' : click, 'show' : show, 'impressions' : impressions.impressions }
 
