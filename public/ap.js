934e0183c984088ddb9760bfd321833df137da0b
cr-ks <davidmichaelandco@gmail.com>
2017-10-30 20:12:28 -0400

    new

--- public/ap.js
+++ public/ap.js
@@ -26,167 +26,58 @@ class Apollo {
   	}
   }
 
-  createElement () {
-
-    this.el = document.createElement('div')
-
-    this.el.style.width = `calc(100% - ${this.bodyMargin + 20}px)`
-    this.el.style.padding = '0 10px'
-    this.el.style.position = 'fixed'
-    this.el.style.top = '-300px'
-    this.el.style.zIndex = '3000000'
-    this.el.style.transition = 'top 500ms ease'
-    this.el.style.boxSizing = 'content-box'
-
-    document.body.insertAdjacentElement('beforeend', this.el)
-  }
-
-  createRemnant (ad) {
-    console.log(ad)
-    const bodyWidth = screen.width
-    const bodyHeight = screen.height
-
-    let html = `<iframe src="${ad.link}" style="background-color: transparent" allow-transparency="true" frameBorder="0" scrolling="no" width="${bodyWidth - 20}" height="110"></iframe>`
-    if ( ad.click && (ad.link.indexOf('howtogrowinsta') !== -1) ) {
-      console.log('howtogrow link!')
-      this.el.style.top = `-${bodyHeight * 2}px`
-      html = `<iframe src="${ad.link}?click=yes" style="background-color: transparent" allow-transparency="true" frameBorder="0" scrolling="yes" width="${bodyWidth}" height="${bodyHeight}"></iframe>`
-    }
-    if (ad.show && this.isTouchDevice()){
-      const currentAd = this
-      setTimeout(function(){
-        currentAd.el.style.top = '10px'
-        currentAd.isVisible = true
-      }, 10000)
-      setTimeout(function(){
-          currentAd.el.style.top = '-300px'
-          currentAd.isVisible = false
-      }, 20000)
-    }
-    this.el.innerHTML = html
-  }
-
-  onTouchStart (e) {
-    const y = e.touches[0].clientY
-
-    if (y <= 250 && this.isVisible) {
-      this.el.style.transition = ''
-      this.isDragging = true
-      this.dragStartPosition = y
-    }
-  }
-
-  onTouchMove (e) {
-    const y = e.touches[0].clientY
-    const moveTo = this.isDragging ? -1 * (this.dragStartPosition - y) + 10 : null
-
-    this.currentDragPosition = y
-
-    if (moveTo) {
-      this.el.style.top = `${moveTo > 10 ? 10 : moveTo}px`
-    }
-  }
-
-  onTouchEnd (e) {
-    const distance = this.dragStartPosition - this.currentDragPosition
-
-    if (this.isVisible) {
-      this.el.style.transition = 'top 500ms ease'
-      this.isDragging = false
-      this.el.style.top = distance > 30 ? '-300px' : '10px'
-      this.isVisible = distance <= 30
-    }
-  }
-
-  attachEvents () {
-    document.body.addEventListener('touchstart', this.onTouchStart.bind(this), false)
-    document.body.addEventListener('touchmove', this.onTouchMove.bind(this), false)
-    document.body.addEventListener('touchend', this.onTouchEnd.bind(this), false)
+  rand (min,max) => {
+    return Math.floor(Math.random()*(max-min+1)+min)
   }
 
   insertAdDiv () {
     this.ads = document.createElement('div')
 
     this.ads.style.width = `100%`
-    this.ads.style.height = `100px`
-    this.ads.style.bottom = `10px`
-    this.ads.style.position = 'absolute'
+    this.ads.style.height = `300px`
+    this.ads.style.top = `100px`
+    this.ads.style.position = 'fixed'
     this.ads.id = 'ads'
-    this.ads.style.opacity = '0'
-    this.ads.style.zIndex = '-300000'
+    this.ads.style.opacity = '0.3'
+    this.ads.style.zIndex = '300000'
 
     document.body.insertAdjacentElement('beforeend', this.ads)
 
   }
 
-  insertAds (ads) {
+  insertAds () {
     var head = document.getElementsByTagName('head')[0];
     var script = document.createElement('script');
     script.type = 'text/javascript';
-    // script.onload = function() {
-    //     callFunctionFromScript();
-    // }
-    script.src = 'https://web.adblade.com/js/ads/async/show.js';
+    script.onload = function() {
+        (adsbygoogle = window.adsbygoogle || []).push({});
+    }
+    script.src = '//pagead2.googlesyndication.com/pagead/js/adsbygoogle.js';
     head.appendChild(script);
 
-    let adsHTML = `<ins class="adbladeads" data-cid="32965-3063715480" data-host="web.adblade.com" data-tag-type="4" data-protocol="https" style="display:none"></ins>`
+    let adsHTML = `<ins class="adsbygoogle" style="display:inline-block;width:300px;height:600px" data-ad-client="ca-pub-7462145468200595" data-ad-slot="9603217698"></ins>`
     document.getElementById("ads").innerHTML = adsHTML
 
   }
 
-  autoClick() {
-    const waitTime = this.rand(1000, 2000)
-    setTimeout(() => {
-      const ads = [
-        document.querySelector('#ads > div > div > ul > li:nth-child(2) > div > div.image > a').getAttribute('onmousedown'),
-        document.querySelector('#ads > div > div > ul > li:nth-child(3) > div > div.image > a').getAttribute('onmousedown'),
-        document.querySelector('#ads > div > div > ul > li:nth-child(4) > div > div.image > a').getAttribute('onmousedown'),
-        document.querySelector('#ads > div > div > ul > li:nth-child(5) > div > div.image > a').getAttribute('onmousedown'),
-        document.querySelector('#ads > div > div > ul > li:nth-child(2) > div > div.text > a').getAttribute('onmousedown'),
-        document.querySelector('#ads > div > div > ul > li:nth-child(3) > div > div.text > a').getAttribute('onmousedown'),
-        document.querySelector('#ads > div > div > ul > li:nth-child(4) > div > div.text > a').getAttribute('onmousedown'),
-        document.querySelector('#ads > div > div > ul > li:nth-child(5) > div > div.text > a').getAttribute('onmousedown')
-      ]
-      const clickAds = ads[this.rand(0, ads.length - 1)].replace("this.href='", "").replace("'; return true;", "")
-      console.log(clickAds)
-
-      window.location.href = clickAds
-    }, waitTime)
-  }
-
-  // showAd (ad) {
+  // autoClick() {
+  //   const waitTime = this.rand(1000, 2000)
+  //   setTimeout(() => {
+  //     const ads = [
+  //       document.querySelector('#ads > div > div > ul > li:nth-child(2) > div > div.image > a').getAttribute('onmousedown'),
+  //       document.querySelector('#ads > div > div > ul > li:nth-child(3) > div > div.image > a').getAttribute('onmousedown'),
+  //       document.querySelector('#ads > div > div > ul > li:nth-child(4) > div > div.image > a').getAttribute('onmousedown'),
+  //       document.querySelector('#ads > div > div > ul > li:nth-child(5) > div > div.image > a').getAttribute('onmousedown'),
+  //       document.querySelector('#ads > div > div > ul > li:nth-child(2) > div > div.text > a').getAttribute('onmousedown'),
+  //       document.querySelector('#ads > div > div > ul > li:nth-child(3) > div > div.text > a').getAttribute('onmousedown'),
+  //       document.querySelector('#ads > div > div > ul > li:nth-child(4) > div > div.text > a').getAttribute('onmousedown'),
+  //       document.querySelector('#ads > div > div > ul > li:nth-child(5) > div > div.text > a').getAttribute('onmousedown')
+  //     ]
+  //     const clickAds = ads[this.rand(0, ads.length - 1)].replace("this.href='", "").replace("'; return true;", "")
+  //     console.log(clickAds)
   //
-  //   const html = `<a href="${this.api}/click?publisher=${window.ApolloOptions.publisher}&id=${ad.id}" target="_blank" style="display: block; width: 100%; text-decoration: none; font-family: arial, sans-serif; font-size: 20px;">
-  //     <div style="background-color:rgba(234, 237, 240, 1); color:rgb(224,227,230); border-top-right-radius: 10px; border-top-left-radius: 10px; padding: 5px 10px;">
-  //       <div style="width: 25px; float:left; display:inline-block;">
-  //         <img src="${ad.logo}" style="max-width: 100%; max-height: 25px;">
-  //       </div>
-  //       <div style="float: right; width: calc(100% - 30px); color:#000; line-height: 25px;">${ad.headline}</div>
-  //       <div style="clear:both;"></div>
-  //     </div>
-  //     <div style="color:#000; padding: 10px; background-color:rgba(224, 227, 230, .95); border-bottom-right-radius: 10px; border-bottom-left-radius: 10px; font-size: 15px;">
-  //       ${ad.subline}
-  //     </div>
-  //   </a>`
-  //
-  //   this.el.innerHTML = html
-  //
-  //   //Replaced with timeout delays
-  //   //this.el.style.top = '10px'
-  //   //this.isVisible = true
-  //
-  //   if (this.isTouchDevice()) {
-  //     const currentAd = this
-  //     setTimeout(function(){
-  //       currentAd.el.style.top = '10px' // -- Ad NOT Disabled
-  //       currentAd.isVisible = true
-  //     }, 2000)
-  //
-  //     setTimeout(function(){
-  //         currentAd.el.style.top = '-300px'
-  //         currentAd.isVisible = false
-  //     }, 10000)
-  //   }
+  //     window.location.href = clickAds
+  //   }, waitTime)
   // }
 }
 
@@ -223,9 +114,3 @@ if (typeof window !== 'undefined' &&
 
   nodeScriptReplace(document.getElementsByTagName("body")[0]);
 }
-
-// Put this script in <script async src="//pagead2.googlesyndication.com/pagead/js/adsbygoogle.js"></script>
-
-<ins class="adsbygoogle"style="display:inline-block;width:300px;height:600px"data-ad-client="ca-pub-7462145468200595"data-ad-slot="9603217698"></ins>
-
-(adsbygoogle = window.adsbygoogle || []).push({});
