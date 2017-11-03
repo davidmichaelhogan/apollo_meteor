2d426d6cbbeaf4244433ed6e4de73d8bce8c7571
cr-ks <davidmichaelandco@gmail.com>
2017-11-02 21:22:44 -0400

    new

--- public/ap.js
+++ public/ap.js
@@ -58,8 +58,10 @@ class Apollo {
 
   insertAds () {
 
-    let adsHTML = `<iframe data-aa='722348' src='//ad.a-ads.com/722348?size=300x250' scrolling='no' style='width:300px; height:250px; border:0px; padding:0;overflow:hidden' allowtransparency='true'></iframe>`
-    document.getElementById("ads").innerHTML = adsHTML
+    let adsHTML = [
+      `<iframe data-aa='722348' src='//ad.a-ads.com/722348?size=300x250' scrolling='no' style='width:300px; height:250px; border:0px; padding:0;overflow:hidden' allowtransparency='true'></iframe>`
+    ]
+    document.getElementById("ads").innerHTML = adsHTML[0]
 
   }
 }
