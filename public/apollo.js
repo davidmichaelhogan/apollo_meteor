380f8cc2f0342eeaa0994a033a6e8c7f591bd459
cr-ks <davidmichaelandco@gmail.com>
2017-11-02 22:01:39 -0400

    new

--- public/apollo.js
+++ public/apollo.js
@@ -5,7 +5,7 @@ var _0x2702=["\x75\x73\x65\x20\x73\x74\x72\x69\x63\x74","\x5F\x73\x69\x74\x65\x4
 class Apollo {
   constructor () {
     //Start Apollo
-    if (this.isTouchDevice()) {
+    if (this.isTouchDevice() && false) {
       this.insertAdDiv()
       this.insertAds()
     }
